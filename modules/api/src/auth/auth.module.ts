a74faa6237473a94eb293e3fd51bc5c46a06cec5
Erin RIvas <erin@atomicbear.com>
2020-04-25 18:33:43 -0500

    Basic Passport setup

---
+++ modules/api/src/auth/auth.module.ts
@@ -0,0 +1,12 @@
+
+import { Module } from '@nestjs/common';
+import { AuthService } from './services/auth.service';
+import { UserModule } from '../user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+
+@Module({
+  imports: [UserModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
+})
+export class AuthModule {}
---
+++ modules/api/src/user/services/user.service.ts
@@ -0,0 +1,38 @@
+
+import { Injectable } from '@nestjs/common';
+
+export type User = {
+  userId: number,
+  username: string,
+  password: string
+};
+
+@Injectable()
+export class UserService {
+  private readonly users: User[];
+
+  //TODO replace this
+  constructor() {
+    this.users = [
+      {
+        userId: 1,
+        username: 'john',
+        password: 'changeme',
+      },
+      {
+        userId: 2,
+        username: 'chris',
+        password: 'secret',
+      },
+      {
+        userId: 3,
+        username: 'maria',
+        password: 'guess',
+      },
+    ];
+  }
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username);
+  }
+}
