2ff9b34f79aedcf90740600f8525615dd5a2a7f8
Erin RIvas <erin@atomicbear.com>
2020-06-07 13:27:16 -0500

    Cleanup of unused config settings

--- modules/api/src/config.ts
+++
@@ -1,8 +0,0 @@
-
-export default () => ({
-  port: Number(process.env.PORT) || 3000,
-  db: {
-    host: process.env.DATABASE_HOST || 'localhost',
-    port: Number(process.env.DATABASE_PORT) || 5432,
-  },
-});
