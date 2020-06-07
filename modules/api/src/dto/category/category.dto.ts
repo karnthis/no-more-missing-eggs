eccee4f987b7006c16ebf789fe007cb4fdee6e9a
Erin RIvas <erin@atomicbear.com>
2020-06-07 13:31:49 -0500

    Updating DTOs

--- modules/api/src/dto/category/category.dto.ts
+++ modules/api/src/dto/category/category.dto.ts
@@ -1,12 +1,13 @@
 import {ApiProperty} from '@nestjs/swagger';
 import {ItemDto} from '../item/item.dto';
 import {CreateKitchenDto} from '../kitchen/create-kitchen.dto';
-import {IsNotEmpty, ValidateNested} from 'class-validator';
+import {IsAlpha, IsNotEmpty, ValidateNested} from 'class-validator';
 
 export class CategoryDto {
 
     @ApiProperty()
     @IsNotEmpty()
+    @IsAlpha()
     name: string;
 
     @ApiProperty()
--- modules/api/src/dto/kitchen/update-kitchen.dto.ts
+++ modules/api/src/dto/kitchen/update-kitchen.dto.ts
@@ -1,10 +1,11 @@
 import {ApiProperty} from '@nestjs/swagger';
-import {IsNotEmpty, IsOptional} from 'class-validator';
+import {IsAlpha, IsNotEmpty, IsOptional} from 'class-validator';
 
 export class UpdateKitchenDto {
 
   @ApiProperty()
   @IsNotEmpty()
+  @IsAlpha()
   // @IsOptional()
   kitchenName: string;
 
