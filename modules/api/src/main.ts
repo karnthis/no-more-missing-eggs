1271ea8d015aca348cab1b5a744d2c6f1d992d50
Erin RIvas <erin@atomicbear.com>
2020-11-25 21:04:17 -0600

    adding CORS

--- modules/api/src/main.ts
+++ modules/api/src/main.ts
@@ -6,6 +6,7 @@ import {ValidationPipe} from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
